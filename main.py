4cefacd623659d34fb0991a5178cc340cd2401e1
Aaditya-373 <aadityakannan123@gmail.com>
2025-07-26 23:51:13 +0530

    thruga voice test

--- main.py
+++ main.py
@@ -100,11 +100,11 @@ async def process_guild(guild):
     while voice.is_playing():
         await asyncio.sleep(1)
 
-    # member = guild.get_member(random.choice(user_ids))
-    # if member and member.voice:
-    #     print(f"Kicking {member.display_name} from {active_channel.name}")
-    #     await member.move_to(None)
-    #     await asyncio.sleep(1)
+    member = guild.get_member(random.choice(user_ids))
+    if member and member.voice:
+        print(f"Kicking {member.display_name} from {active_channel.name}")
+        await member.move_to(None)
+        await asyncio.sleep(1)
 
     await voice.disconnect()
     print(f"Completed action in {guild.name}")
@@ -114,7 +114,7 @@ async def process_guild(guild):
 async def on_ready():
     print(f"Bot logged in as {bot.user}")
     await asyncio.sleep(10)
-    # join_play_disconnect.start()
+    join_play_disconnect.start()
 
 
 @bot.event
